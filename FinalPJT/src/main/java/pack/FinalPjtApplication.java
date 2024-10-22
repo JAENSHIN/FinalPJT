1fece07a47c2bd29e067887977267f10e35d6a4f
JAENSHIN <aiaiai200205@gmail.com>
2024-10-22 12:09:16 +0900

    기본 프로젝트 파

---
+++ FinalPJT/src/main/java/pack/FinalPjtApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalPjtApplication.class, args);
+		System.out.print("HEllo World");	}
+
+}
