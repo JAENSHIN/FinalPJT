0426d82cc7c82306a559de6477999daed00bb167
sunho <yunsh0613@skuniv.ac.kr>
2024-10-22 12:25:48 +0900

    test

--- FinalPJT/src/main/java/pack/FinalPjtApplication.java
+++ FinalPJT/src/main/java/pack/FinalPjtApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinalPjtApplication {
-
+//
 	public static void main(String[] args) {
 		SpringApplication.run(FinalPjtApplication.class, args);
 	}
